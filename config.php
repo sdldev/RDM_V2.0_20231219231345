09e9184d3b8631f8223cc774c3c69d11785b985c
sdldev <sidolapakdev@gmail.com>
2024-06-10 09:24:51 +0700

    cek

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-// $host = "192.168.100.30";
-// $databaseuser = "root";
-// $databasename = "rdm";
-// $dbpassword = "casaos";
-$host = getenv('DB_SERVER');
-$databaseuser = getenv('DB_USERNAME');
-$databasename = getenv('DB_DATABASE');
-$dbpassword = getenv('DB_PASSWORD');
+$host = "localhost";
+$databaseuser = "rdm";
+$databasename = "rdm";
+$dbpassword = "rdm12!!";
+// $host = getenv('DB_SERVER');
+// $databaseuser = getenv('DB_USERNAME');
+// $databasename = getenv('DB_DATABASE');
+// $dbpassword = getenv('DB_PASSWORD');
 define("DB_SERVER",$host);
 define("DB_USERNAME",$databaseuser);
 define("DB_PASSWORD",$dbpassword);
