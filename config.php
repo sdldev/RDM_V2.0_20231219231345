4494432df36156879324c769574b71426a33ffb6
sdldev <sidolapakdev@gmail.com>
2024-06-10 09:31:37 +0700

    DevCoder\DotEnv

--- config.php
+++ config.php
@@ -1,13 +1,19 @@
 <?php
+use DevCoder\DotEnv;
+$absolutePathToEnvFile = __DIR__ . '/.env';
+(new DotEnv($absolutePathToEnvFile))->load();
+
 defined('BASEPATH') OR exit('No direct script access allowed');
-$host = "localhost";
-$databaseuser = "rdm";
-$databasename = "rdm";
-$dbpassword = "rdm12!!";
-// $host = getenv('DB_SERVER');
-// $databaseuser = getenv('DB_USERNAME');
-// $databasename = getenv('DB_DATABASE');
-// $dbpassword = getenv('DB_PASSWORD');
+
+
+// $host = "localhost";
+// $databaseuser = "rdm";
+// $databasename = "rdm";
+// $dbpassword = "rdm12!!";
+$host = getenv('DB_SERVER');
+$databaseuser = getenv('DB_USERNAME');
+$databasename = getenv('DB_DATABASE');
+$dbpassword = getenv('DB_PASSWORD');
 define("DB_SERVER",$host);
 define("DB_USERNAME",$databaseuser);
 define("DB_PASSWORD",$dbpassword);
