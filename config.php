54a88e2fbef7d5fcc6e7eeb0db32372ad2996e75
sdldev <sidolapakdev@gmail.com>
2024-06-10 07:56:55 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$host = "192.168.100.30";
+$databaseuser = "root";
+$databasename = "rdm";
+$dbpassword = "casaos";
+define("DB_SERVER",$host);
+define("DB_USERNAME",$databaseuser);
+define("DB_PASSWORD",$dbpassword);
+define("DB_DATABASE",$databasename);
+?>
