bcaa628129b729810086393dd21b569063ad189c
sdldev <sidolapakdev@gmail.com>
2024-06-10 08:27:30 +0700

    user passw to env

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$host = "192.168.100.30";
-$databaseuser = "root";
-$databasename = "rdm";
-$dbpassword = "casaos";
+// $host = "192.168.100.30";
+// $databaseuser = "root";
+// $databasename = "rdm";
+// $dbpassword = "casaos";
+$host = env('DB_SERVER');
+$databaseuser = env('DB_USERNAME');
+$databasename = env('DB_DATABASE');
+$dbpassword = env('DB_PASSWORD');
 define("DB_SERVER",$host);
 define("DB_USERNAME",$databaseuser);
 define("DB_PASSWORD",$dbpassword);
